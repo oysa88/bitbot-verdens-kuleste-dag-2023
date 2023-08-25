c210f7357e304b8395aa4108590db699dd6eb97d
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2023-08-25 13:56:18 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,67 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        bitbot.rotate(BBRobotDirection.Left, 60)
+        bitbot.setLedColor(0xFF8000)
+    } else if (receivedNumber == 2) {
+        bitbot.rotate(BBRobotDirection.Right, 60)
+        bitbot.setLedColor(0x00FF00)
+    } else if (receivedNumber == 3) {
+        bitbot.go(BBDirection.Forward, 100)
+        bitbot.setLedColor(0x0000FF)
+    } else if (receivedNumber == 4) {
+        bitbot.go(BBDirection.Reverse, 100)
+        bitbot.setLedColor(0xFF00FF)
+    } else {
+        bitbot.setLedColor(0xFF0000)
+    }
+})
+radio.setGroup(1)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `, 0)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `, 0)
+    } else if (input.rotation(Rotation.Pitch) < -25) {
+        radio.sendNumber(3)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `, 0)
+    } else if (input.rotation(Rotation.Pitch) > 25) {
+        radio.sendNumber(4)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `, 0)
+    } else {
+        radio.sendNumber(0)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `, 0)
+    }
+})
